d199f037f5dc04a4117fadaf627c58bdc84a0ac1
FindMeSomeFun <129369873+FindMeSomeFun@users.noreply.github.com>
2023-06-11 19:02:38 +0200

    Theme switch

--- resources/code.js
+++ resources/code.js
@@ -1,7 +1,21 @@
 var repo = 'https://raw.githubusercontent.com/FindMeSomeFun/gtnh-questbook-wiki/main/';
 
-//var url = new URL(window.location.href);
-//var getParam = url.searchParams.get('foo'); 
+var url = new URL(window.location.href);
+
+var param = url.searchParams.get('light');
+if (param == 1) {
+	document.getElementById('lights-on').checked = true;
+	switchTheme();
+}
+
+param = url.searchParams.get('version');
+alert(document.getElementById('availableVersions').value);
+if (param != null) {
+	alert(param);
+	document.getElementById('versions').value = param;
+} else {
+	alert('Version: ' + param + 'does not exist, fallback to latest.')
+}
 //alert(url + ' -> ' + getParam);
 readTextFile(repo + 'resources/versions.txt', 'versions');
 readTextFile(repo + 'resources/2.3.0/questLines.txt', 'questLines');
@@ -27,15 +41,18 @@ function populateElement(text, id) {
 	var lines = text.split("\n");
 	
 	if (id == 'versions') {
+		var values = ""
 		for (var i = 0; i < lines.length; i++) {
 			if (lines[i] != '') {
+				values += lines[i];
 				element.innerHTML += '<option value="' + lines[i]  + '">' + lines[i] + '</option>';
 			}
 		}
+		document.getElementById('availableVersions').value = values;
 	} else if (id == 'questLines') {
 		for (var i = 0; i < lines.length; i++) {
 			if (lines[i] != '') {
-				element.innerHTML += '<a href="#" class="questLine"><img src="' + repo + 'resources/image/item/' + lines[i++] + '.png" alt="No Image"> <span> ' + lines[i] + '</span> </a>';
+				element.innerHTML += '<div class="questLine"><img src="' + repo + 'resources/image/item/' + lines[i++] + '.png" alt="No Image"> <span> ' + lines[i] + '</span> </div>';
 			}
 		}
 	}
@@ -55,5 +72,12 @@ window.onclick = function(event) {
 				openDropdown.classList.remove('show');
 			}
 		}
+	}
+}
+
+function switchTheme(event) {
+	var elems = document.getElementsByClassName('theme-switch');
+	for (var i = 0; i < elems.length; i++) {
+		elems[i].classList.toggle("light");
 	}
 }
